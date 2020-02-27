2d7b40a3f845bfd9a54aa6e72790d3a2e4db11f7
Zyra Amar <Zyra_Amar@gmail.com>
2020-02-27 10:04:34 +0800

    Create calc.php

---
+++ AMAR_ZYRA JANE L/calc.php
@@ -0,0 +1,16 @@
+
+<form action="solution.php" method="POST">
+     <input type="text" name="fnum" placeholder="1st Number">
+	 <br>
+	 <input type="text" name="1num" placeholder="2nd Number">
+	 <br><td><select name="operator">
+	     <option>None</option>
+	     <option value="+">Addition</option>
+		 <option value="-">Subtract</option>
+		 <option value="*">Multiply</option>
+		 <option value="/">Divide</option>
+		 
+	</select>	
+	<br>
+	<button type="submit" name="submit" value="submit">Calculate</button>
+</form>
