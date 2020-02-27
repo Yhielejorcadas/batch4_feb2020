7eb7b114e6bf8c9c93e2e6c4c244de8b0aafbe87
Zyra Amar <Zyra_Amar@gmail.com>
2020-02-27 10:04:43 +0800

    Create solution.php

---
+++ AMAR_ZYRA JANE L/solution.php
@@ -0,0 +1,25 @@
+<?php
+$first_num = $_POST['fnum'];
+$second_num = $_POST['1num'];
+$operator = $_POST['operator'];
+$result = '';
+if (is_numeric ($first_num) && is_numeric($second_num)){
+		 switch ($operator) {
+			 case "+":
+			    echo $result = $first_num + $second_num;
+				break;
+			 case "-":
+			    echo $result = $first_num - $second_num;
+				break;
+			  case "*":
+			    echo $result = $first_num * $second_num;
+				break;
+			  case "/":
+			    echo $result = $first_num / $second_num;
+		 }			
+				
+	 }	 
+   
+   
+?>
+     
